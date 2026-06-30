b53793a92ffe9bf451972ec71f4d9c67ce567f22
aminmohammadkhan <amin.mohammadkhan@gmail.com>
2026-06-30 17:00:19 +0330

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ def run_worker():
     'https://www.googleapis.com/auth/drive'
 ]
     # خواندن دسترسی از متغیر محیطی (بدون نیاز به فایل)
-    creds_dict = json.loads(os.environ['GOOGLE_CREDENTIALS'])
-    creds = service_account.Credentials.from_service_account_info(creds_dict, scopes=scopes)
+    creds = service_account.Credentials.from_service_account_info('credentials.json', scopes=scopes)
     gc = gspread.authorize(creds)
     
     # نام شیت خود را اینجا بنویس
